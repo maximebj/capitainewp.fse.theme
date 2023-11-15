87d35d454e210e088141c7b8a64b75f220adb139
Maxime BJ <maxime@dysign.fr>
2023-11-15 10:19:41 +0100

    theme.json fully operationnal + font + theme features

--- inc/config.php
+++ inc/config.php
@@ -2,23 +2,22 @@
 
 namespace Wia;
 
-add_action( 'init', 'Wia\\themeFeatures' );
 add_action( 'wp_enqueue_scripts', 'Wia\\registerAssets' );
 
+# Retirer le pattern directory et la suggestion de blocs
+remove_action( 'enqueue_block_editor_assets', 'wp_enqueue_editor_block_directory_assets' );
+remove_theme_support( 'core-block-patterns' );
 
-function themeFeatures() {
-  add_theme_support( 'post-thumbnails' );
+# Ajouter des fonctionnalités
+add_theme_support( 'post-thumbnails' );
 
-  add_theme_support( 'editor-styles' );
-  add_editor_style( [ 'editor.css' ] );
+add_theme_support( 'editor-styles' );
+add_editor_style( [ 'editor.css' ] );
 
-  # Retirer le pattern directory et la suggestion de blocs
-  remove_action( 'enqueue_block_editor_assets', 'wp_enqueue_editor_block_directory_assets' );
-  remove_theme_support( 'core-block-patterns' );
-}
 
 function registerAssets() {
-
+  # Enqueue styles
+  wp_enqueue_style( 'main', get_stylesheet_uri(), [], '1.0.0' );
 
   # Disable native blocks styles
   wp_dequeue_style( 'wp-block-columns' );
