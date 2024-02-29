36bc5b3747d06ee42e2de084ea219277a205aa4c
Maxime BJ <maxime@dysign.fr>
2024-02-29 11:42:41 +0100

    Removed old blocks file

--- functions.php
+++ functions.php
@@ -5,7 +5,6 @@ namespace Capitaine;
 # Include files
 require_once __DIR__ . '/inc/config.php';
 require_once __DIR__ . '/inc/editor.php';
-require_once __DIR__ . '/inc/blocks.php';
 require_once __DIR__ . '/inc/post-types.php';
 
 # Init Classes
