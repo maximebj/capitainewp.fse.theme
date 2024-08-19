d92dd7be566770ebb3b49bf3f4b94008b4f40b1f
Maxime BJ <maxime@dysign.fr>
2024-08-19 13:51:00 +0200

    Blocks Styles Variations

--- functions.php
+++ functions.php
@@ -1,40 +1,63 @@
 <?php
 
 # Retirer les accents des noms de fichiers
-add_filter('sanitize_file_name', 'remove_accents');
+add_filter("sanitize_file_name", "remove_accents");
 
 # Retirer le pattern directory et la suggestion de blocs
-remove_action('enqueue_block_editor_assets', 'wp_enqueue_editor_block_directory_assets');
-remove_theme_support('core-block-patterns');
+remove_action("enqueue_block_editor_assets", "wp_enqueue_editor_block_directory_assets");
+remove_theme_support("core-block-patterns");
 
 # Ajouter des fonctionnalités
-add_theme_support('title-tag');
-add_theme_support('post-thumbnails');
-add_theme_support('editor-styles');
+add_theme_support("title-tag");
+add_theme_support("post-thumbnails");
+add_theme_support("editor-styles");
 
 # Déclarer les scripts et les styles
 function capitaine_register_assets()
 {
   # Enqueue styles
-  wp_enqueue_style('main', get_stylesheet_uri(), [], '1.0.0');
+  wp_enqueue_style("main", get_stylesheet_uri(), [], "1.0.0");
 
   # Disable native blocks styles
-  wp_dequeue_style('wp-block-columns');
+  wp_dequeue_style("wp-block-columns");
 }
-add_action('wp_enqueue_scripts', 'capitaine_register_assets');
+add_action("wp_enqueue_scripts", "capitaine_register_assets");
 
 # On enregistre une variation de style de bloc personnalisée pour le bloc bouton
 function capitaine_register_block_styles()
 {
-  register_block_style(
-    'core/button',
-    [
-      'name' => 'action-button',
-      'label' => __("Bouton d’action", 'capitainewp'),
-    ]
-  );
+  // register_block_style(
+  //   "core/button",
+  //   [
+  //     "name" => "primary-button",
+  //     "label" => __("Action", "capitainewp"),
+  //   ]
+  // );
+
+  // register_block_style(
+  //   "core/image",
+  //   [
+  //     "name" => "secondary-button",
+  //     "label" => __("Secondaire", "capitainewp"),
+  //   ]
+  // );
 }
-add_action('init', 'capitaine_register_block_styles');
+add_action("init", "capitaine_register_block_styles");
+
+# Charger les styles des variations de blocs
+function capitaine_register_blocks_assets()
+{
+  // wp_enqueue_block_style(
+  //   "core/button",
+  //   [
+  //     "handle" => "capitaine-button",
+  //     "src"    => get_theme_file_uri("assets/css/button.css"),
+  //     "path"   => get_theme_file_path("assets/css/button.css"),
+  //     "ver"    => "1.0",
+  //   ]
+  // );
+}
+add_action("init", "capitaine_register_blocks_assets");
 
 # On retire le CSS natif des colonnes et on ajoute notre propre feuille de style
 function capitaine_deregister_stylesheets()
@@ -44,44 +67,44 @@ function capitaine_deregister_stylesheets()
 
   # On ajoute notre propre feuille de style
   wp_enqueue_block_style(
-    'columns',
+    "columns",
     [
-      'handle' => "capitaine-columns",
-      'src' => get_theme_file_uri("css/columns.css"),
-      'path' => get_theme_file_path("css/columns.css"),
-      'ver' => "1.0",
+      "handle" => "capitaine-columns",
+      "src" => get_theme_file_uri("css/columns.css"),
+      "path" => get_theme_file_path("css/columns.css"),
+      "ver" => "1.0",
     ]
   );
 }
-add_action('wp_enqueue_scripts', 'capitaine_deregister_stylesheets');
+add_action("wp_enqueue_scripts", "capitaine_deregister_stylesheets");
 
 # Ajout de catégories de compositions personnalisées
 function capitaine_register_block_pattern_categories(): void
 {
-  register_block_pattern_category('cta', ['label' => "Call to action"]);
-  register_block_pattern_category('cards', ['label' => "Cards"]);
-  register_block_pattern_category('marketing', ['label' => "Marketing"]);
+  register_block_pattern_category("cta", ["label" => "Call to action"]);
+  register_block_pattern_category("cards", ["label" => "Cards"]);
+  register_block_pattern_category("marketing", ["label" => "Marketing"]);
 }
-add_action('init', 'capitaine_register_block_pattern_categories');
+add_action("init", "capitaine_register_block_pattern_categories");
 
 # Allow SVG and WebP uploads
 function capitaine_allow_mime($mimes)
 {
-  $mimes['svg'] = 'image/svg+xml';
-  $mimes['webp'] = 'image/webp';
+  $mimes["svg"] = "image/svg+xml";
+  $mimes["webp"] = "image/webp";
 
   return $mimes;
 }
-add_filter('upload_mimes', 'capitaine_allow_mime');
+add_filter("upload_mimes", "capitaine_allow_mime");
 
 # Allow WebP uploads
 function capitaine_allow_file_types($types, $file, $filename, $mimes)
 {
-  if (false !== strpos($filename, '.webp')) {
-    $types['ext'] = 'webp';
-    $types['type'] = 'image/webp';
+  if (false !== strpos($filename, ".webp")) {
+    $types["ext"] = "webp";
+    $types["type"] = "image/webp";
   }
 
   return $types;
 }
-add_filter('wp_check_filetype_and_ext', 'capitaine_allow_file_types', 10, 4);
+add_filter("wp_check_filetype_and_ext", "capitaine_allow_file_types", 10, 4);
