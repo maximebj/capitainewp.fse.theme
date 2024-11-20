b61e6b9c1736806215be0e9535025412fe5492fa
Maxime BJ <maxime@dysign.fr>
2024-11-20 15:46:32 +0100

    Auto loading custom blocks CSS

--- functions.php
+++ functions.php
@@ -22,7 +22,8 @@ function capitaine_register_assets()
 add_action("wp_enqueue_scripts", "capitaine_register_assets");
 
 
-# Charger les styles des variations de blocs
+# Charger les styles de blocs
+/*
 function capitaine_register_blocks_assets()
 {
     wp_enqueue_block_style(
@@ -36,23 +37,38 @@ function capitaine_register_blocks_assets()
     );
 }
 add_action("init", "capitaine_register_blocks_assets");
+*/
+
+# Charger automatiquement les styles de blocs
+function capitaine_auto_register_blocks_assets()
+{
+    # Trouver les fichiers CSS 
+    $files = glob(get_template_directory() . '/assets/styles/*.css');
+
+    foreach ($files as $file) {
+
+        # Récupérer le nom du fichier et trouver le bloc correspondant
+        $filename   = basename($file, '.css');
+        $block_name = str_replace('core-', 'core/', $filename);
+
+        # Déclarer le style
+        wp_enqueue_block_style(
+            $block_name,
+            [
+                'handle' => "capitaine-{$filename}",
+                "src"    => get_theme_file_uri("assets/styles/{$filename}.css"),
+                "path"   => get_theme_file_path("assets/styles/{$filename}.css"),
+                "ver"    => filemtime(get_theme_file_path("assets/styles/style.css"))
+            ]
+        );
+    }
+}
+add_action('init', 'capitaine_auto_register_blocks_assets');
 
 # On retire le CSS natif des colonnes et on ajoute notre propre feuille de style
 function capitaine_deregister_stylesheets()
 {
-    # On retire le CSS natif des colonnes
     wp_dequeue_style("wp-block-columns");
-
-    # On ajoute notre propre feuille de style
-    wp_enqueue_block_style(
-        "columns",
-        [
-            "handle" => "capitaine-columns",
-            "src" => get_theme_file_uri("css/columns.css"),
-            "path" => get_theme_file_path("css/columns.css"),
-            "ver" => "1.0",
-        ]
-    );
 }
 add_action("wp_enqueue_scripts", "capitaine_deregister_stylesheets");
 
@@ -214,172 +230,3 @@ function capitaine_register_post_types()
     register_taxonomy('type-projets', 'portfolio', $args);
 }
 add_action('init', 'capitaine_register_post_types');
-
-
-// TODO REMOVE
-class SetupEditor
-{
-    # Extracted JSON configuration data
-    protected array $config;
-
-    # All in one setup
-    public function setup()
-    {
-        # Hooks
-        add_action('init', [$this, 'registerCustomBlocks']);
-        add_action('init', [$this, 'registerBlocksAssets']);
-
-        # Setup
-        $this->setupEditorConfig();
-    }
-
-    # Auto register custom blocks from blocks/* 
-    public function registerCustomBlocks()
-    {
-        $folders = glob(get_template_directory() . '/blocks/*/');
-
-        foreach ($folders as $folder) {
-            $block = basename($folder);
-            register_block_type(dirname(__DIR__) . "/blocks/$block");
-        }
-    }
-
-    # Auto load blocks styles from assets/css/*
-    public function registerBlocksAssets()
-    {
-        $files = glob(get_template_directory() . '/assets/css/*.css');
-
-        foreach ($files as $file) {
-            $filename   = basename($file, '.css');
-            $block_name = str_replace('core-', 'core/', $filename);
-
-            wp_enqueue_block_style(
-                $block_name,
-                [
-                    'handle' => "capitaine-{$filename}",
-                    'src'    => get_theme_file_uri("assets/css/{$filename}.css"),
-                    'path'   => get_theme_file_path("assets/css/{$filename}.css"),
-                    'ver'    => wp_get_environment_type() === 'local' ? filemtime($file) : null,
-                ]
-            );
-        }
-    }
-
-    # Load editor custom configuration and register configurations
-    public function setupEditorConfig()
-    {
-        # Load editor JSON configuration file
-        $this->config = $this->loadJsonConfig();
-
-        # Loop through configuration and launch corresponding function
-        foreach ($this->config as $key => $data) {
-
-            switch ($key) {
-                case 'registerBlocksStyles':
-                    add_action('init', [$this, 'registerBlocksStyles']);
-                    break;
-                case 'registerBlocksCategories':
-                    add_filter('block_categories_all', [$this, 'registerBlocksCategories']);
-                    break;
-                case 'registerPatternsCategories':
-                    add_action('init', [$this, 'registerPatternsCategories']);
-                    break;
-                case 'deregisterBlocks':
-                    add_filter('allowed_block_types_all', [$this, 'deregisterBlocks']);
-                    break;
-                case 'deregisterStylesheets':
-                    add_action('wp_enqueue_scripts', [$this, 'deregisterStylesheets']);
-                    break;
-                case 'deregisterBlocksStyles':
-                    add_action('enqueue_block_editor_assets', [$this, 'deregisterBlocksStyles']);
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-
-    # Register new block styles variations
-    function registerBlocksStyles()
-    {
-        $block_styles = $this->getConfigData('registerBlocksStyles');
-
-        foreach ($block_styles as $block_name => $styles) {
-            foreach ($styles as $style_name => $style_label) {
-                register_block_style(
-                    $block_name,
-                    [
-                        'name' => $style_name,
-                        'label' => $style_label,
-                    ]
-                );
-            }
-        }
-    }
-
-    # Register custom blocks categories
-    public function registerBlocksCategories($categories): array
-    {
-        $block_categories_to_register = $this->getConfigData('registerBlocksCategories');
-
-        # New categories will appear at the top
-        $new_categories = [];
-
-        foreach ($block_categories_to_register as $slug => $title) {
-            $new_categories[] = [
-                'slug' => $slug,
-                'title' => $title,
-                'icon' => null
-            ];
-        }
-
-        return array_merge($new_categories, $categories);
-    }
-
-    # Register new block patterns categories
-    public function registerPatternsCategories()
-    {
-        $patterns = $this->getConfigData('registerPatternsCategories');
-        foreach ($patterns as $name => $label) {
-            $category = ['label' => $label];
-            register_block_pattern_category($name, $category);
-        }
-    }
-
-    # Deregister unwanted blocks stylesheets in order to register custom ones
-    public function deregisterStylesheets()
-    {
-        $blocks_stylesheets_to_disable = $this->getConfigData('deregisterStylesheets');
-
-        foreach ($blocks_stylesheets_to_disable as $block) {
-            $handle = str_replace('core/', '', $block);
-            wp_dequeue_style("wp-block-$handle");
-        }
-    }
-
-    # Load the Json file
-    protected function loadJsonConfig(): array
-    {
-        # Check existence of config.json file
-        if (!file_exists(get_template_directory() . '/config.json')) {
-            return [];
-        }
-
-        # Extract data from file
-        $config = json_decode(file_get_contents(get_template_directory() . '/config.json'), true);
-
-        # Check if data is valid
-        if (!is_array($config)) {
-            throw new \Exception('config.json file is not valid');
-        }
-
-        # Return all data
-        return $config;
-    }
-
-    # Get config specific data 
-    protected function getConfigData($key): array
-    {
-        return $this->config[$key] ?? [];
-    }
-}
