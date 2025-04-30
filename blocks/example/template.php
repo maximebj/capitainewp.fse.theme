77da027c5c4e88acf1c4b67c8fdbbd024b56b33b
Maxime BJ <maxime@dysign.fr>
2025-04-30 14:33:35 +0200

    Added block link container

--- blocks/example/template.php
+++ blocks/example/template.php
@@ -1,12 +1,12 @@
 <?php
-$class = isset($block['className']) ? ' ' . $block['className'] : '';
+$attributes = get_block_wrapper_attributes();
 
 # Récupérer les données du bloc
 $title = get_field('title');
 $content = get_field('content');
 
 ?>
-<div class="wp-block-example<?php echo $class; ?>">
+<div <?php echo $attributes; ?>>
   <h2><?php echo $title; ?></h2>
   <?php echo $content; ?>
 </div>
